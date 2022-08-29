97c31842f6fe40c4981313f8195ca638e1c5d040
luoty <luoty@asiainfo.com>
2022-08-29 11:45:46 +0800

    添加部分配置内容

--- vue.config.js
+++ vue.config.js
@@ -1,15 +1,18 @@
 
 'use strict'
 const path = require('path')
-
 // Gzip压缩需要的内容
 // const CompressionPlugin = require('compression-webpack-plugin')
+const port = process.env.port || 9999 // dev port
+
+const cdn = {
+  css: [],
+  js: []
+}
 
 function resolve(dir) {
   return path.join(__dirname, dir)
 }
-
-const port = process.env.port || 9999 // dev port
 
 module.exports = {
   publicPath: '',
@@ -57,5 +60,93 @@ module.exports = {
         },
       },
     }
+  },
+  chainWebpack(config) {
+    // when there are many pages, it will cause too many meaningless requests
+    config.plugins.delete('prefetch')
+    config.plugins.delete('preload')
+
+    // set svg-sprite-loader
+    config.module
+      .rule('svg')
+      .exclude.add(resolve('src/icons'))
+      .end()
+    config.module
+      .rule('icons')
+      .test(/\.svg$/)
+      .include.add(resolve('src/icons'))
+      .end()
+      .use('svg-sprite-loader')
+      .loader('svg-sprite-loader')
+      .options({
+        symbolId: 'icon-[name]'
+      })
+      .end()
+
+    // set preserveWhitespace
+    config.module
+      .rule('vue')
+      .use('vue-loader')
+      .loader('vue-loader')
+      .tap(options => {
+        options.compilerOptions.preserveWhitespace = true
+        return options
+      })
+      .end()
+    
+    // https://webpack.js.org/configuration/devtool/#development
+    config
+      .when(process.env.NODE_ENV === 'development',
+        config => config.devtool('cheap-source-map')
+      )
+    
+    config
+      .when(process.env.NODE_ENV !== 'development',
+        config => {
+          config
+            .plugin('ScriptExtHtmlWebpackPlugin')
+            .after('html')
+            .use('script-ext-html-webpack-plugin', [{
+            // `runtime` must same as runtimeChunk name. default is `runtime`
+              inline: /runtime\..*\.js$/
+            }])
+            .end()
+          config
+            .optimization.splitChunks({
+              chunks: 'all',
+              cacheGroups: {
+                libs: {
+                  name: 'chunk-libs',
+                  test: /[\\/]node_modules[\\/]/,
+                  priority: 10,
+                  chunks: 'initial' // only package third parties that are initially dependent
+                },
+                commons: {
+                  name: 'chunk-commons',
+                  test: resolve('src/components'), // can customize your rules
+                  minChunks: 3, //  minimum common number
+                  priority: 5,
+                  reuseExistingChunk: true
+                }
+              }
+            })
+          config.optimization.runtimeChunk('single')
+        }
+      )
+
+    if (process.env.NODE_ENV !== 'development') {
+      config.plugin('html').tap(args => {
+        args[0].cdn = cdn // 配置CDN列表
+        return args
+      })
+    }
   }
+  // css: {
+  //   loaderOptions: {
+  //     sass: {
+  //       // data V8-、 prependData V8、additionalData V10+
+  //       additionalData: '@import "@/styles/index.scss"' // 全局引入
+  //     }
+  //   }
+  // }
 }
